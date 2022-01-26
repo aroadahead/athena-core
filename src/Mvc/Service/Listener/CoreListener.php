01fc94e61e821f4039fb798f0d6c69ce42b15fe5
yonatonreid <yonatonreid@gmail.com>
2022-01-26 03:23:19 -0500

    upgrade to athena packages

--- src/Mvc/Service/Listener/CoreListener.php
+++ src/Mvc/Service/Listener/CoreListener.php
@@ -3,7 +3,9 @@
 namespace AthenaCore\Mvc\Service\Listener;
 
 use Laminas\EventManager\EventManagerInterface;
+use Laminas\Json\Json;
 use Laminas\Mvc\MvcEvent;
+use Poseidon\Poseidon;
 
 class CoreListener extends AbstractServiceListener
 {
@@ -19,7 +21,6 @@ class CoreListener extends AbstractServiceListener
     public function onRoute(MvcEvent $e): void
     {
         $routeMatch = $e -> getRouteMatch();
-        $routeMatch = $e -> getRouteMatch();
         $routeMatchedName = $routeMatch -> getMatchedRouteName();
         $realController = $routeMatch -> getParam('controller');
         $className = str_replace(['\\', '_Controller_'], '_', $realController);
@@ -29,8 +30,40 @@ class CoreListener extends AbstractServiceListener
         $module = $moduleParts[0];
         $controller = $moduleParts[1];
         $action = $routeMatch -> getParam('action');
-        $this -> container -> get('log') -> info("Dispatching route module: {$module}");
-        $this -> container -> get('log') -> info("Dispatching route controller: {$controller}");
-        $this -> container -> get('log') -> info("Dispatching route action: {$action}");
+        $log = $this -> container -> get('log');
+        $log -> info("Dispatching route matchedName: {$routeMatchedName}");
+        $log -> info("Dispatching route module: {$module}");
+        $log -> info("Dispatching route controller: {$controller}");
+        $log -> info("Dispatching route action: {$action}");
+        $log -> info("Params: " . Json ::encode($routeMatch -> getParams()));
+
+        $registry = Poseidon ::registry();
+        if (!$registry -> has('app.route.matchedName')) {
+            $registry -> add('app.route.matchedName', $routeMatchedName);
+        }
+        if (!$registry -> has('app.route.params')) {
+            $registry -> add('app.route.params', $routeMatch -> getParams());
+        }
+        if (!$registry -> has('app.route.fullRouteController')) {
+            $registry -> add('app.route.fullRouteController', $realController);
+        }
+        if (!$registry -> has('app.route.module')) {
+            $registry -> add('app.route.module', $module);
+        }
+        if (!$registry -> has('app.route.controller')) {
+            $registry -> add('app.route.controller', $controller);
+        }
+        if (!$registry -> has('app.route.action')) {
+            $registry -> add('app.route.action', $action);
+        }
+        $serverVars = ['UNIQUE_ID', 'HTTP_USER_AGENT', 'SERVER_ADDR', 'REMOTE_ADDR', 'REQUEST_TIME'];
+        foreach ($serverVars as $var) {
+            $key = "app.server." . strtolower(str_replace('_', '.', $var));
+            $val = $e -> getRequest() -> getServer($var);
+            if (!$registry -> has($key)) {
+                $registry -> add($key, $val);
+            }
+            $log -> info("Server $var:  $val");
+        }
     }
 }
