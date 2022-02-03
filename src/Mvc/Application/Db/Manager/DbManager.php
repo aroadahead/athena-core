d954ee0375262efe67b002e8a881af4d344e21e0
yonatonreid <yonatonreid@gmail.com>
2022-02-03 00:15:38 -0500

    upgrade to athena packages

--- src/Mvc/Application/Db/Manager/DbManager.php
+++ src/Mvc/Application/Db/Manager/DbManager.php
@@ -6,6 +6,7 @@ namespace AthenaCore\Mvc\Application\Db\Manager;
 
 use AthenaCore\Mvc\Application\Application\Manager\ApplicationManager;
 use Laminas\Db\Adapter\Adapter;
+use Laminas\Db\TableGateway\Feature\GlobalAdapterFeature;
 
 class DbManager extends ApplicationManager
 {
@@ -17,6 +18,8 @@ class DbManager extends ApplicationManager
         $config = $this -> applicationCore -> getConfigManager() -> lookup('db');
         $this -> masterConnection = new ConnectionHandler($config -> master);
         $this -> slaveConnection = new ConnectionHandler($config -> slave);
+
+        GlobalAdapterFeature::setStaticAdapter($this->masterAdapter());
     }
 
     public function init(): void
