3db4bd71b22599b0eb1bb34c446701664bbf9e83
yonatonreid <yonatonreid@gmail.com>
2022-02-07 01:04:53 -0500

    upgrade to athena packages

--- src/Mvc/Service/MvcService.php
+++ src/Mvc/Service/MvcService.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace AthenaCore\Mvc\Service;
 
+use Psr\Container\ContainerInterface;
+
 /**
  * Abstract Mvc service
  */
 abstract class MvcService
 {
+    public function __construct(protected ContainerInterface $container)
+    {
+    }
+
     public function hello():string
     {
         return 'hello';
