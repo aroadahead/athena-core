e892762c45283ce88aad6ad4841513ce77a21230
yonatonreid <yonatonreid@gmail.com>
2022-02-07 00:59:34 -0500

    upgrade to athena packages

---
+++ src/Mvc/Application/Laminas/ModuleServiceLoader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AthenaCore\Mvc\Application\Laminas;
+
+use AthenaCore\Mvc\Service\MvcService;
+use Poseidon\Data\DataObject;
+use Psr\Container\ContainerInterface;
+
+class ModuleServiceLoader
+{
+    protected ?DataObject $modulesLoaded = null;
+
+    public function __construct(protected ContainerInterface $container)
+    {
+        $this -> modulesLoaded = new DataObject();
+    }
+
+    public function load(string $moduleService): MvcService
+    {
+        if (!$this -> modulesLoaded -> hasItem($moduleService)) {
+            $moduleServiceClazz = $this -> container -> get('module.service.' . $moduleService);
+            $this -> modulesLoaded -> setItem($moduleService, $moduleServiceClazz);
+        }
+        return $this -> modulesLoaded -> getItem('module.service.' . $moduleService);
+    }
+}
