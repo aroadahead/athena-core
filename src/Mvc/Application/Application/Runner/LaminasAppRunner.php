0d0ea57691bc53cdde8f72da5fcfafe8c76f4cb7
yonatonreid <yonatonreid@gmail.com>
2022-03-08 04:05:17 -0500

    npm react import existing project

--- src/Mvc/Application/Application/Runner/LaminasAppRunner.php
+++ src/Mvc/Application/Application/Runner/LaminasAppRunner.php
@@ -13,11 +13,11 @@ class LaminasAppRunner extends ApplicationCore
 
     public function deploy():void
     {
-        $this -> userManager -> addJsLocalStorageItem('locale', $this -> environmentManager -> getDefaultLocale());
-        $this -> userManager -> addJsLocalStorageItem('pubKey', '6asd68d68ddd6saadd79asd7das79ads9');
-        $this -> environmentManager -> addJsLocalStorageItem('version', $this -> environmentManager -> getVersionNumber());
-        $this -> environmentManager -> addJsLocalStorageItem('dist', $this -> environmentManager -> getVersionName());
-        $this -> configManager -> facade() -> addJsLocalStorageItem('configItem', 'someVal');
+//        $this -> userManager -> addJsLocalStorageItem('locale', $this -> environmentManager -> getDefaultLocale());
+//        $this -> userManager -> addJsLocalStorageItem('pubKey', '6asd68d68ddd6saadd79asd7das79ads9');
+//        $this -> environmentManager -> addJsLocalStorageItem('version', $this -> environmentManager -> getVersionNumber());
+//        $this -> environmentManager -> addJsLocalStorageItem('dist', $this -> environmentManager -> getVersionName());
+//        $this -> configManager -> facade() -> addJsLocalStorageItem('configItem', 'someVal');
 
         $laminasConfig = $this -> configManager -> facade() -> laminasConfig();
         $appConfig = $laminasConfig -> application -> toArray();
